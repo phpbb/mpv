a753a64fcdc417615b03283cc7a65895af9eb899
maelsoucaze <maelsoucaze@gmail.com>
2010-10-16 17:28:54 +0200

    Copyright notice has been changed.

--- includes/languages/fr_x_tu/lang.php
+++ includes/languages/fr_x_tu/lang.php
@@ -2,7 +2,7 @@
 /**
 *
 * This file is part of French (Casual Honorifics) MPV translation.
-* Copyright (C) 2010 phpBB.fr
+* Copyright (C) 2010 Maël Soucaze
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 * MPV language [French (Casual Honorifics)]
 *
 * @package   mpv
-* @author    Maël Soucaze <maelsoucaze@phpbb.fr> (Maël Soucaze) http://www.phpbb.fr/
+* @author    Maël Soucaze <maelsoucaze@gmail.com> http://mael.soucaze.com/
 * @copyright (c) 2010 phpBB Group
 * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License
 * @version   $Id$
