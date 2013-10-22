6c606d3d2bcb6ecc68184305726cd9e81ffee5a8
Marc Alexander <admin@m-a-styles.de>
2013-10-22 15:09:15 +0200

    [ticket/63771] Add tests for MODx license and rename tests to modx_test

---
+++ tests/code/modx_test.php
@@ -0,0 +1,143 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+require_once dirname(__FILE__) . '/../../includes/tests/tests_modx.php';
+
+class phpbb_modx_test extends phpbb_test_case
+{
+	private $test;
+
+	public static function provider()
+	{
+		return array(
+			array('1.0.0', false, true),
+			array('1.0.0-pl1', false, true),
+			array('1.0.0-b1', 'MAJOR_VERSION_UNSTABLE', false),
+			array('0.1.0', 'MAJOR_VERSION_UNSTABLE', false),
+			array('1.0.0 update to 1.0.1', 'MAJOR_VERSION_UNSTABLE', false),
+			array('1.0.0a1', 'MAJOR_VERSION_UNSTABLE', false),
+			array('1.0.0foobar', 'MAJOR_VERSION_UNSTABLE', false),
+			array('foobar', 'INVALID_VERSION_FORMAT', false),
+			array('2.15.3', false, true),
+			array('19.1.2', false, true),
+			array('1.2.15', false, true),
+		);
+	}
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->test = new mpv_tests_modx(new mpv());
+		$this->test->modx_object = new phpbb_mock_modx_object();
+		$this->version = new phpbb_mock_modx_version();
+	}
+
+	/**
+	* @dataProvider provider
+	*/
+	public function test_version($test, $expected_error, $expected_result)
+	{
+		global $user;
+
+		if ($expected_error !== false)
+		{
+			$this->setExpectedTriggerError(E_USER_ERROR, $expected_error);
+		}
+
+		$this->version->value = $test;
+		$this->test->modx_object->set_xpath($this->version);
+		$result = $this->test->unittest('test_version', array());
+
+		$this->assertEquals($expected_result, $result);
+	}
+
+	public function test_version_no_object()
+	{
+		$this->setExpectedTriggerError(E_USER_ERROR, 'VERSION_FIELD_MISSING');
+		$this->version = '1.0.0';
+		$this->test->modx_object->set_xpath($this->version);
+		$result = $this->test->unittest('test_version', array());
+		$this->assertEquals(false, $result);
+	}
+
+	public static function data_modx_license()
+	{
+		return array(
+			array('http://opensource.org/licenses/gpl-license.php GNU General Public License v2', false, true),
+			array('http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2', false, true),
+			array('http://opensource.org/licenses/MIT', 'LICENSE_NOT_GPL2', false),
+			array(null, 'LICENSE_FIELD_MISSING', false),
+		);
+	}
+
+	/**
+	* @dataProvider data_modx_license
+	*/
+	public function test_modx_license($test, $expected_error, $expected_result)
+	{
+		global $user;
+
+		if ($expected_error !== false)
+		{
+			$this->setExpectedTriggerError(E_USER_ERROR, $expected_error);
+		}
+
+		if (isset($test))
+		{
+			$this->license = new phpbb_mock_modx_license();
+			$this->license->value = $test;
+		}
+		else
+		{
+			$this->license = $test;
+		}
+		$this->test->modx_object->set_by_name('license', $this->license);
+		$result = $this->test->unittest('test_license', array());
+
+		$this->assertEquals($expected_result, $result);
+	}
+}
+
+class phpbb_mock_modx_object
+{
+	private $xpath;
+
+	private $data_ary;
+
+	public function get_xpath($foo, $bar = true)
+	{
+		return $this->xpath;
+	}
+
+	public function set_xpath($foo)
+	{
+		$this->xpath = $foo;
+	}
+
+	public function get_by_name($name, $return = true)
+	{
+		return (isset($this->data_ary[$name])) ? $this->data_ary[$name] : false;
+	}
+
+	public function set_by_name($name, $value)
+	{
+		$this->data_ary[$name] = $value;
+	}
+}
+
+class phpbb_mock_modx_version
+{
+	public $value;
+}
+
+class phpbb_mock_modx_license
+{
+	public $value;
+}
